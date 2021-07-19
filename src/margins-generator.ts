cb4a926309bd8f2e37f308768d82583cd4868dac
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-19 18:52:01 +0200

    step: generate trbl margins

--- src/margins-generator.ts
+++ src/margins-generator.ts
@@ -30,6 +30,26 @@ export class Margins implements GeneratorInterface {
       output += `*[data-my='${key}'] {\n  margin-top: ${value};\n  margin-bottom: ${value};\n}\n`;
     }
 
+    // Top margin: data-mt="*"
+    for (const [key, value] of Object.entries(this.spacing)) {
+      output += `*[data-mt='${key}'] {\n  margin-top: ${value};\n}\n`;
+    }
+
+    // Right margin: data-mr="*"
+    for (const [key, value] of Object.entries(this.spacing)) {
+      output += `*[data-mr='${key}'] {\n  margin-right: ${value};\n}\n`;
+    }
+
+    // Bottom margin: data-mb="*"
+    for (const [key, value] of Object.entries(this.spacing)) {
+      output += `*[data-mb='${key}'] {\n  margin-bottom: ${value};\n}\n`;
+    }
+
+    // Left margin: data-ml="*"
+    for (const [key, value] of Object.entries(this.spacing)) {
+      output += `*[data-ml='${key}'] {\n  margin-left: ${value};\n}\n`;
+    }
+
     return output;
   }
 
