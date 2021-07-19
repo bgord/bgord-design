8770404364ddbf67accbfcecf25cf130df203725
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-07-19 19:43:03 +0200

    step: add minify css step

--- scripts/prepare-css-files.sh
+++ scripts/prepare-css-files.sh
@@ -9,8 +9,12 @@ node dist/index.js
 info "Copying normalize.css to dist..."
 cp src/normalize.css dist/
 
-info "Running design system CSS through postcss..."
+info "Running postcss..."
 npx postcss dist/main.css -o dist/main.css
 
 info "Running doiuse..."
 npx doiuse --browsers " last 2 versions" dist/main.css
+
+info "Minifying..."
+npx minify --css dist/main.css > dist/main.min.css
+npx minify --css dist/normalize.css > dist/normalize.min.css
