2f10e2f166cbd6c755f7e278fbf5e600efa7b16b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-08-03 23:22:24 +0200

    step: add positions generator

---
+++ src/postions-generator.ts
@@ -0,0 +1,32 @@
+import { GeneratorInterface, GeneratorConfigType } from './generator';
+
+export class PositionsGenerator implements GeneratorInterface {
+  spacing: GeneratorConfigType['spacing'];
+  displays: GeneratorConfigType['displays'];
+  positions: GeneratorConfigType['positions'];
+
+  constructor(config: GeneratorConfigType) {
+    this.spacing = config.spacing;
+    this.displays = config.displays;
+    this.positions = config.positions;
+  }
+
+  generateHeader(): string {
+    return '/* Positions */\n\n';
+  }
+
+  generateCss(): string {
+    let output = '';
+
+    // Regular display: data-position="*"
+    for (const [key, value] of Object.entries(this.displays)) {
+      output += `*[data-position='${key}'] {\n  position: ${value};\n}\n`;
+    }
+
+    return output;
+  }
+
+  generateFooter(): string {
+    return '/* ===================== */\n\n';
+  }
+}
