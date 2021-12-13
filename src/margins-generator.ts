0a5c738164f6097f838e65911a705402132d03ea
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-13 20:16:58 +0100

    feature: add mobile margins

--- src/margins-generator.ts
+++ src/margins-generator.ts
@@ -2,9 +2,11 @@ import { GeneratorInterface, GeneratorConfigType } from './generator';
 
 export class Margins implements GeneratorInterface {
   spacing: GeneratorConfigType['spacing'];
+  breakpoints: GeneratorConfigType['breakpoints'];
 
   constructor(config: GeneratorConfigType) {
     this.spacing = config.spacing;
+    this.breakpoints = config.breakpoints;
   }
 
   generateHeader(): string {
@@ -14,39 +16,66 @@ export class Margins implements GeneratorInterface {
   generateCss(): string {
     let output = '';
 
-    // Regular margin: data-m="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-m='${key}'] {\n  margin: ${value};\n}\n`;
     }
 
-    // Horizontal margin: data-mx="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-mx='${key}'] {\n  margin-left: ${value};\n  margin-right: ${value};\n}\n`;
     }
 
-    // Vertical margin: data-my="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-my='${key}'] {\n  margin-top: ${value};\n  margin-bottom: ${value};\n}\n`;
     }
 
-    // Top margin: data-mt="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-mt='${key}'] {\n  margin-top: ${value};\n}\n`;
     }
 
-    // Right margin: data-mr="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-mr='${key}'] {\n  margin-right: ${value};\n}\n`;
     }
 
-    // Bottom margin: data-mb="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-mb='${key}'] {\n  margin-bottom: ${value};\n}\n`;
     }
 
-    // Left margin: data-ml="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-ml='${key}'] {\n  margin-left: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-m='${key}'] {\n    margin: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-mx='${key}'] {\n    margin-left: ${value};\n    margin-right: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-my='${key}'] {\n    margin-top: ${value};\n    margin-bottom: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-mt='${key}'] {\n    margin-top: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-mr='${key}'] {\n    margin-right: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-mb='${key}'] {\n    margin-bottom: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-ml='${key}'] {\n    margin-left: ${value};\n  }\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
