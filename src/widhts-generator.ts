7ffe7c8836ac68880db7a6632af716ab1c1cd5cb
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-13 22:13:48 +0100

    feature: add mobile widths

--- src/widhts-generator.ts
+++ src/widhts-generator.ts
@@ -2,9 +2,11 @@ import { GeneratorInterface, GeneratorConfigType } from './generator';
 
 export class WidthsGenerator implements GeneratorInterface {
   widhts: GeneratorConfigType['widths'];
+  breakpoints: GeneratorConfigType['breakpoints'];
 
   constructor(config: GeneratorConfigType) {
     this.widhts = config.widths;
+    this.breakpoints = config.breakpoints;
   }
 
   generateHeader(): string {
@@ -17,6 +19,16 @@ export class WidthsGenerator implements GeneratorInterface {
     // Regular display: data-width="*"
     for (const [key, value] of Object.entries(this.widhts)) {
       output += `*[data-width='${key}'] {\n  width: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.widhts)) {
+        output += `  *[data-${name}-width='${key}'] {\n    width: ${value};\n  }\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
