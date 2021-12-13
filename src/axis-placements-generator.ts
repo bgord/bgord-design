9a48d9a8fe02543b92c47a598a1d85cdbb1298b8
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-13 22:25:58 +0100

    feature: add mobile axis placements

--- src/axis-placements-generator.ts
+++ src/axis-placements-generator.ts
@@ -2,9 +2,11 @@ import { GeneratorInterface, GeneratorConfigType } from './generator';
 
 export class AxisPlacementsGenerator implements GeneratorInterface {
   axisPlacements: GeneratorConfigType['axisPlacements'];
+  breakpoints: GeneratorConfigType['breakpoints'];
 
   constructor(config: GeneratorConfigType) {
     this.axisPlacements = config.axisPlacements;
+    this.breakpoints = config.breakpoints;
   }
 
   generateHeader(): string {
@@ -22,6 +24,20 @@ export class AxisPlacementsGenerator implements GeneratorInterface {
     // Cross axis placement: data-cross="*"
     for (const [key, value] of Object.entries(this.axisPlacements)) {
       output += `*[data-cross='${key}'] {\n  align-items: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.axisPlacements)) {
+        output += `  *[data-${name}-main='${key}'] {\n    justify-content: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.axisPlacements)) {
+        output += `  *[data-${name}-cross='${key}'] {\n    align-items: ${value};\n  }\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
