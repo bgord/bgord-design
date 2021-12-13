8ce4fde10353a2f11fd1ad791964789f52fc254a
Bartosz Gordon <bartosz.gordon@sealcode.org>
2021-12-13 20:25:44 +0100

    feature: add mobile paddings

--- src/paddings-generator.ts
+++ src/paddings-generator.ts
@@ -2,9 +2,11 @@ import { GeneratorInterface, GeneratorConfigType } from './generator';
 
 export class Paddings implements GeneratorInterface {
   spacing: GeneratorConfigType['spacing'];
+  breakpoints: GeneratorConfigType['breakpoints'];
 
   constructor(config: GeneratorConfigType) {
     this.spacing = config.spacing;
+    this.breakpoints = config.breakpoints;
   }
 
   generateHeader(): string {
@@ -47,6 +49,40 @@ export class Paddings implements GeneratorInterface {
     // Left padding: data-pl="*"
     for (const [key, value] of Object.entries(this.spacing)) {
       output += `*[data-pl='${key}'] {\n  padding-left: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-p='${key}'] {\n    padding: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-px='${key}'] {\n    padding-left: ${value};\n    padding-right: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-py='${key}'] {\n    padding-top: ${value};\n    padding-bottom: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-pt='${key}'] {\n    padding-top: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-pr='${key}'] {\n    padding-right: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-pb='${key}'] {\n    padding-bottom: ${value};\n  }\n`;
+      }
+
+      for (const [key, value] of Object.entries(this.spacing)) {
+        output += `  *[data-${name}-pl='${key}'] {\n    padding-left: ${value};\n  }\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
