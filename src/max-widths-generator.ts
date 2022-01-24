19d3747ab0e884f1cd4f247891905b63525f5d1e
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-01-24 21:43:59 +0100

    feature: add max-width breakpoints

--- src/max-widths-generator.ts
+++ src/max-widths-generator.ts
@@ -2,9 +2,11 @@ import { GeneratorInterface, GeneratorConfigType } from './generator';
 
 export class MaxWidthsGenerator implements GeneratorInterface {
   maxWidths: GeneratorConfigType['maxWidths'];
+  breakpoints: GeneratorConfigType['breakpoints'];
 
   constructor(config: GeneratorConfigType) {
     this.maxWidths = config.maxWidths;
+    this.breakpoints = config.breakpoints;
   }
 
   generateHeader(): string {
@@ -17,6 +19,16 @@ export class MaxWidthsGenerator implements GeneratorInterface {
     // Regular display: data-max-width="*"
     for (const [key, value] of Object.entries(this.maxWidths)) {
       output += `*[data-max-width='${key}'] {\n  max-width: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.maxWidths)) {
+        output += `*[data-${name}-max-width='${key}'] {\n  max-width: ${value};\n}\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
