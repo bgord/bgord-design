3a59ceb708be2573ec4843d5fbc46cd7c95d698b
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-02-05 13:44:35 +0100

    feature: add responsive displays

--- src/displays-generator.ts
+++ src/displays-generator.ts
@@ -2,9 +2,11 @@ import { GeneratorInterface, GeneratorConfigType } from './generator';
 
 export class DisplaysGenerator implements GeneratorInterface {
   displays: GeneratorConfigType['displays'];
+  breakpoints: GeneratorConfigType['breakpoints'];
 
   constructor(config: GeneratorConfigType) {
     this.displays = config.displays;
+    this.breakpoints = config.breakpoints;
   }
 
   generateHeader(): string {
@@ -21,6 +23,20 @@ export class DisplaysGenerator implements GeneratorInterface {
       } else {
         output += `*[data-display='${key}'] {\n  display: ${value};\n}\n`;
       }
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.displays)) {
+        if (value === 'flex') {
+          output += `*[data-${name}-display='${key}'] {\n  display: ${value};\nflex-wrap: wrap;\n}\n`;
+        } else {
+          output += `*[data-${name}-display='${key}'] {\n  display: ${value};\n}\n`;
+        }
+      }
+
+      output += `}\n`;
     }
 
     return output;
