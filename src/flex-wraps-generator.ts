74de85d4f892244978f0b6ec7df055aec77308c5
Bartosz Gordon <bartosz.gordon@sealcode.org>
2022-08-22 23:55:39 +0200

    step: add responsive flex-wraps

--- src/flex-wraps-generator.ts
+++ src/flex-wraps-generator.ts
@@ -3,10 +3,13 @@ import { AbstractGenerator, GeneratorConfigType } from './generator';
 export class FlexWrapGenerator extends AbstractGenerator {
   flexWraps: GeneratorConfigType['flexWraps'];
 
+  breakpoints: GeneratorConfigType['breakpoints'];
+
   constructor(config: GeneratorConfigType) {
     super('Flex wraps');
 
     this.flexWraps = config.flexWraps;
+    this.breakpoints = config.breakpoints;
   }
 
   generateCss(): string {
@@ -15,6 +18,16 @@ export class FlexWrapGenerator extends AbstractGenerator {
     // Regular display: data-wrap="*"
     for (const [key, value] of Object.entries(this.flexWraps)) {
       output += `*[data-wrap='${key}'] {\n  flex-wrap: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.flexWraps)) {
+        output += `  *[data-${name}-wrap='${key}'] {\n    flex-wrap: ${value};\n  }\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
