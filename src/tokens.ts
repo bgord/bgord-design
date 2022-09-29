63fd365e1d23ff37e087b48a63c2443015d27f10
Bartosz Gordon <gordonbartosz@gmail.com>
2022-09-29 19:38:00 +0200

    feature: add 4 and 6 to letter spacing

--- src/tokens.ts
+++ src/tokens.ts
@@ -241,13 +241,15 @@ export const Breakpoints: Record<BreakpointType, number> = {
   md: 768,
 };
 
-type LetterSpacingType = '0.5' | '1' | '1.5' | '2';
+type LetterSpacingType = '0.5' | '1' | '1.5' | '2' | '4' | '6';
 
 export const LetterSpacings: Record<LetterSpacingType, number> = {
   '0.5': 0.5,
   '1': 1,
   '1.5': 1.5,
   '2': 2,
+  '4': 4,
+  '6': 6,
 };
 
 type FlexGrowType = '1' | 'unset';
