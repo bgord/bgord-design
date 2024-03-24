3c8b173ce7f9965ae5f4679d76fae15ea55eab3d
Bartosz Gordon <gordonbartosz@gmail.com>
2024-03-24 19:16:10 +0100

    fix: add mobile positions

--- src/positions-generator.ts
+++ src/positions-generator.ts
@@ -3,10 +3,13 @@ import { AbstractGenerator, GeneratorConfigType } from './generator';
 export class PositionsGenerator extends AbstractGenerator {
   positions: GeneratorConfigType['positions'];
 
+  breakpoints: GeneratorConfigType['breakpoints'];
+
   constructor(config: GeneratorConfigType) {
     super('Positions');
 
     this.positions = config.positions;
+    this.breakpoints = config.breakpoints;
   }
 
   generateCss(): string {
@@ -15,6 +18,16 @@ export class PositionsGenerator extends AbstractGenerator {
     // Regular display: data-position="*"
     for (const [key, value] of Object.entries(this.positions)) {
       output += `*[data-position='${key}'] {\n  position: ${value};\n}\n`;
+    }
+
+    for (const [name, value] of Object.entries(this.breakpoints)) {
+      output += `@media (max-width: ${value}px) {\n`;
+
+      for (const [key, value] of Object.entries(this.positions)) {
+        output += `  *[data-${name}-position='${key}'] {\n    position: ${value};\n  }\n`;
+      }
+
+      output += `}\n`;
     }
 
     return output;
