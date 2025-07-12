0b3fff43c9f0ec3aabf737624dc93b2ade78a3d3
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-12 15:40:59 +0200

    refactor:

--- scripts/prepare-css-files.sh
+++ scripts/prepare-css-files.sh
@@ -4,25 +4,25 @@
 source bgord-scripts/base.sh
 
 info "Building design system CSS..."
-node dist/index.js
+bun dist/index.js
 
 info "Copying normalize.css to dist..."
 cp src/normalize.css dist/
 
 info "Running postcss..."
-npx postcss dist/main.css -o dist/main.css
-npx postcss dist/normalize.css -o dist/normalize.css
+bunx postcss dist/main.css -o dist/main.css
+bunx postcss dist/normalize.css -o dist/normalize.css
 
 info "Running doiuse..."
-npx doiuse --browsers ">1%" --ignore "css-touch-action,css-appearance,css-media-resolution,css-resize" dist/main.css
+bunx doiuse --browsers ">1%" --ignore "css-touch-action,css-appearance,css-media-resolution,css-resize" dist/main.css
 
 info "Minifying..."
-npx clean-css-cli dist/main.css -o dist/main.min.css
-npx clean-css-cli dist/normalize.css -o dist/normalize.min.css
+bunx clean-css-cli dist/main.css -o dist/main.min.css
+bunx clean-css-cli dist/normalize.css -o dist/normalize.min.css
 
 info "Compressing..."
-npx gzip dist/main.min.css --extension=gz --extension=br
-npx gzip dist/normalize.min.css --extension=gz --extension=br
+bunx gzip dist/main.min.css --extension=gz --extension=br
+bunx gzip dist/normalize.min.css --extension=gz --extension=br
 
 info "main.css file sizes:"
 # shellcheck disable=SC2010
