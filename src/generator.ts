d2ed6b00339e3cf4e87bfaaada29f9f6ccdc9ff4
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-12 16:15:58 +0200

    refactor: simplify GeneratorConfigType

--- src/generator.ts
+++ src/generator.ts
@@ -18,38 +18,5 @@ export abstract class AbstractGenerator {
 }
 
 export type GeneratorConfigType = {
-  Spacing: typeof Tokens.Spacing;
-  Displays: typeof Tokens.Displays;
-  AxisPlacements: typeof Tokens.AxisPlacements;
-  Positions: typeof Tokens.Positions;
-  FlexWraps: typeof Tokens.FlexWraps;
-  ZIndexes: typeof Tokens.ZIndexes;
-  Widths: typeof Tokens.Widths;
-  FontSizes: typeof Tokens.FontSizes;
-  FontWeights: typeof Tokens.FontWeights;
-  LineHeights: typeof Tokens.LineHeights;
-  FlexDirections: typeof Tokens.FlexDirections;
-  Grayscale: typeof Tokens.Grayscale;
-  Greens: typeof Tokens.Greens;
-  Reds: typeof Tokens.Reds;
-  Oranges: typeof Tokens.Oranges;
-  Breakpoints: typeof Tokens.Breakpoints;
-  LetterSpacings: typeof Tokens.LetterSpacings;
-  FlexGrows: typeof Tokens.FlexGrows;
-  FlexShrinks: typeof Tokens.FlexShrinks;
-  BorderWidths: typeof Tokens.BorderWidths;
-  BorderColors: typeof Tokens.BorderColors;
-  BorderRadiuses: typeof Tokens.BorderRadiuses;
-  MaxWidths: typeof Tokens.MaxWidths;
-  Transforms: typeof Tokens.Transforms;
-  Overflows: typeof Tokens.Overflows;
-  Heights: typeof Tokens.Heights;
-  Cursors: typeof Tokens.Cursors;
-  PointerEvents: typeof Tokens.PointerEvents;
-  Backdrops: typeof Tokens.Backdrops;
-  ObjectFits: typeof Tokens.ObjectFits;
-  ObjectPositions: typeof Tokens.ObjectPositions;
-  Rotates: typeof Tokens.Rotates;
-  MaxHeights: typeof Tokens.MaxHeights;
-  Shadows: typeof Tokens.Shadows;
+  [Key in keyof typeof Tokens]: (typeof Tokens)[Key];
 };
