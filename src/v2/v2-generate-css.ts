194e9d3d2dce634a17b0a124f7623a53ef061f09
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-25 22:55:31 +0200

    step: define token generators in place

--- src/v2/v2-generate-css.ts
+++ src/v2/v2-generate-css.ts
@@ -1,13 +1,50 @@
 import * as TokenGenerators from "./tokens";
-import { TokenGeneratorProcessor } from "./tokens/token-generator-processor";
 
 class GeneratorProcessor {
   static async process() {
-    let output = "";
+    const BackdropsTokenGenerator = new TokenGenerators.BackdropsTokenGenerator();
+    const BorderWidthTokenGenerator = new TokenGenerators.BorderWidthTokenGenerator();
+    const BrandTokenGenerator = new TokenGenerators.BrandTokenGenerator();
+    const BreakpointTokenGenerator = new TokenGenerators.BreakpointTokenGenerator();
+    const DangerTokenGenerator = new TokenGenerators.DangerTokenGenerator();
+    const FontFamilyTokenGenerator = new TokenGenerators.FontFamilyTokenGenerator();
+    const FontSizeTokenGenerator = new TokenGenerators.FontSizeTokenGenerator();
+    const FontWeightTokenGenerator = new TokenGenerators.FontWeightTokenGenerator();
+    const GrayscaleTokenGenerator = new TokenGenerators.GrayscaleTokenGenerator();
+    const LetterSpacingTokenGenerator = new TokenGenerators.LetterSpacingTokenGenerator();
+    const LineHeightTokenGenerator = new TokenGenerators.LineHeightTokenGenerator();
+    const MotionTokenGenerator = new TokenGenerators.MotionTokenGenerator();
+    const OpacityTokenGenerator = new TokenGenerators.OpacityTokenGenerator();
+    const PositiveTokenGenerator = new TokenGenerators.PositiveTokenGenerator();
+    const RadiusTokenGenerator = new TokenGenerators.RadiusTokenGenerator();
+    const ShadowTokenGenerator = new TokenGenerators.ShadowTokenGenerator();
+    const SpacingTokenGenerator = new TokenGenerators.SpacingTokenGenerator();
+    const WarningTokenGenerator = new TokenGenerators.WarningTokenGenerator();
+    const ZIndexTokenGenerator = new TokenGenerators.ZIndexTokenGenerator();
 
-    output += new TokenGeneratorProcessor(
-      Object.values(TokenGenerators).map((TokenGenerator) => new TokenGenerator()),
-    ).generate();
+    let output = ":root {";
+
+    output += BackdropsTokenGenerator.getTokens();
+    output += BorderWidthTokenGenerator.getTokens();
+    output += BrandTokenGenerator.getTokens();
+    output += BreakpointTokenGenerator.getTokens();
+    output += DangerTokenGenerator.getTokens();
+    output += FontFamilyTokenGenerator.getTokens();
+    output += FontSizeTokenGenerator.getTokens();
+    output += FontWeightTokenGenerator.getTokens();
+    output += GrayscaleTokenGenerator.getTokens();
+    output += LetterSpacingTokenGenerator.getTokens();
+    output += LineHeightTokenGenerator.getTokens();
+    output += MotionTokenGenerator.getTokens();
+    output += OpacityTokenGenerator.getTokens();
+    output += PositiveTokenGenerator.getTokens();
+    output += RadiusTokenGenerator.getTokens();
+    output += ShadowTokenGenerator.getTokens();
+    output += SpacingTokenGenerator.getTokens();
+    output += WarningTokenGenerator.getTokens();
+    output += ZIndexTokenGenerator.getTokens();
+
+    output += "}";
 
     await Bun.file("dist/v2/main.css").write(output);
   }
