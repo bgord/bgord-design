ff0659917c507d121e65646d2982cb2ef0b7ba86
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-25 22:19:30 +0200

    step: extract token generator processor

--- src/v2/tokens/template.ts
+++ src/v2/tokens/template.ts
@@ -27,19 +27,3 @@ export abstract class TokenGenerator {
     return result;
   }
 }
-
-export class TokenGeneratorProcessor {
-  constructor(private readonly generators: TokenGenerator[]) {}
-
-  generate(): string {
-    let result = ":root {";
-
-    for (const generator of this.generators) {
-      result += generator.getTokens();
-    }
-
-    result += "}";
-
-    return result;
-  }
-}
