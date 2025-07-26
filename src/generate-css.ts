4722b08c29d1c20e9c6f0f60ebace23d404f3158
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-26 18:42:04 +0200

    step: add label

--- src/generate-css.ts
+++ src/generate-css.ts
@@ -165,6 +165,7 @@ class GeneratorProcessor {
 
     output += await Bun.file("src/ui/button.css").text();
     output += await Bun.file("src/ui/input.css").text();
+    output += await Bun.file("src/ui/label.css").text();
 
     await Bun.file("dist/main.css").write(output);
 
