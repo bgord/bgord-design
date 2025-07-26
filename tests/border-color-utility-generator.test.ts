3d1810e67f29e36a211f9f64427ff5153ad1b230
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-26 15:22:31 +0200

    step: update tests

--- tests/border-color-utility-generator.test.ts
+++ tests/border-color-utility-generator.test.ts
@@ -19,11 +19,27 @@ describe("BorderColorUtilityGenerator", () => {
     );
 
     const css = generator.css();
-    expect(css).toContain("[data-bc='neutral-900'] { border-color: var(--color-neutral-900); }");
-    expect(css).toContain("[data-bc='brand-500'] { border-color: var(--color-brand-500); }");
-    expect(css).toContain("[data-bc='positive-500'] { border-color: var(--color-positive-500); }");
-    expect(css).toContain("[data-bc='danger-500'] { border-color: var(--color-danger-500); }");
-    expect(css).toContain("[data-bc='warning-500'] { border-color: var(--color-warning-500); }");
+    expect(css).toContain(
+      "[data-bc='neutral-900'] { border-color: var(--color-neutral-900); border-style: solid; }",
+    );
+    expect(css).toContain(
+      "[data-bct='neutral-900'] { border-top-color: var(--color-neutral-900); border-top-style: solid; }",
+    );
+    expect(css).toContain(
+      "[data-bcr='neutral-900'] { border-right-color: var(--color-neutral-900); border-right-style: solid; }",
+    );
+    expect(css).toContain(
+      "[data-bcb='neutral-900'] { border-bottom-color: var(--color-neutral-900); border-bottom-style: solid; }",
+    );
+    expect(css).toContain(
+      "[data-bcl='neutral-900'] { border-left-color: var(--color-neutral-900); border-left-style: solid; }",
+    );
+    expect(css).toContain(
+      "[data-bcx='neutral-900'] { border-left-color: var(--color-neutral-900); border-right-color: var(--color-neutral-900); border-left-style: solid; border-right-style: solid; }",
+    );
+    expect(css).toContain(
+      "[data-bcy='neutral-900'] { border-top-color: var(--color-neutral-900); border-bottom-color: var(--color-neutral-900); border-top-style: solid; border-bottom-style: solid; }",
+    );
 
     const ts = generator.toTypeScript();
     expect(ts).toContain('"neutral-900"');
@@ -31,7 +47,13 @@ describe("BorderColorUtilityGenerator", () => {
     expect(ts).toContain('"positive-500"');
     expect(ts).toContain('"danger-500"');
     expect(ts).toContain('"warning-500"');
-    expect(ts).toMatch(/^"data-bc"\?:/);
+    expect(ts).toContain('"data-bc"?:');
+    expect(ts).toContain('"data-bct"?:');
+    expect(ts).toContain('"data-bcr"?:');
+    expect(ts).toContain('"data-bcb"?:');
+    expect(ts).toContain('"data-bcl"?:');
+    expect(ts).toContain('"data-bcx"?:');
+    expect(ts).toContain('"data-bcy"?:');
   });
 
   test("with overrides", () => {
@@ -50,7 +72,7 @@ describe("BorderColorUtilityGenerator", () => {
     );
 
     const css = generator.css();
-    expect(css).toContain("[data-bc='foo'] { border-color: var(--color-foo); }");
+    expect(css).toContain("[data-bc='foo'] { border-color: var(--color-foo); border-style: solid; }");
 
     const ts = generator.toTypeScript();
     expect(ts).toContain('"foo"');
--- tests/border-width-utility-generator.test.ts
+++ tests/border-width-utility-generator.test.ts
@@ -8,18 +8,26 @@ describe("BorderWidthUtilityGenerator", () => {
     const generator = new BorderWidthUtilityGenerator(BorderWidthTokenGenerator);
 
     const css = generator.css();
-    expect(css).toContain("[data-bw='none'] { border-width: var(--border-width-none); }");
-    expect(css).toContain("[data-bw='hairline'] { border-width: var(--border-width-hairline); }");
-    expect(css).toContain("[data-bw='thin'] { border-width: var(--border-width-thin); }");
     expect(css).toContain("[data-bw='base'] { border-width: var(--border-width-base); }");
-    expect(css).toContain("[data-bw='thick'] { border-width: var(--border-width-thick); }");
-    expect(css).toContain("[data-bw='heavy'] { border-width: var(--border-width-heavy); }");
-    expect(css).toContain("[data-bw='unset'] { border-width: var(--border-width-unset); }");
+    expect(css).toContain("[data-bwt='base'] { border-top-width: var(--border-width-base); }");
+    expect(css).toContain("[data-bwr='base'] { border-right-width: var(--border-width-base); }");
+    expect(css).toContain("[data-bwb='base'] { border-bottom-width: var(--border-width-base); }");
+    expect(css).toContain("[data-bwl='base'] { border-left-width: var(--border-width-base); }");
+    expect(css).toContain(
+      "[data-bwx='base'] { border-left-width: var(--border-width-base); border-right-width: var(--border-width-base); }",
+    );
+    expect(css).toContain(
+      "[data-bwy='base'] { border-top-width: var(--border-width-base); border-bottom-width: var(--border-width-base); }",
+    );
 
     const ts = generator.toTypeScript();
-    expect(ts).toEqualIgnoringWhitespace(`
-      "data-bw"?: "none" | "hairline" | "thin" | "base" | "thick" | "heavy" | "unset";
-    `);
+    expect(ts).toContain('"data-bw"?:');
+    expect(ts).toContain('"data-bwt"?:');
+    expect(ts).toContain('"data-bwr"?:');
+    expect(ts).toContain('"data-bwb"?:');
+    expect(ts).toContain('"data-bwl"?:');
+    expect(ts).toContain('"data-bwx"?:');
+    expect(ts).toContain('"data-bwy"?:');
   });
 
   test("with overrides", () => {
