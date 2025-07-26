b25866a1adcfa7a2a31c31147cabf2ca99fff0eb
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-26 17:29:44 +0200

    step: adjust opacity

--- src/tokens/opacity-token-generator.ts
+++ src/tokens/opacity-token-generator.ts
@@ -2,7 +2,7 @@ import { TokenConfigType, TokenGenerator } from "./template";
 
 export class OpacityTokenGenerator extends TokenGenerator {
   base: TokenConfigType = {
-    "opacity-high": "0.8",
+    "opacity-high": "0.7",
     "opacity-medium": "0.5",
     "opacity-low": "0.2",
   };
