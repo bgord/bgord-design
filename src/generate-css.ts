48a40f34ca29b9b9b8cdf6f035f6f3e77872de10
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-27 13:34:13 +0200

    step: generate interactions

--- src/generate-css.ts
+++ src/generate-css.ts
@@ -181,6 +181,10 @@ class GeneratorProcessor {
     output += await Bun.file("src/animations/grow-fade-in.css").text();
     output += await Bun.file("src/animations/shrink-fade-out.css").text();
 
+    output += await Bun.file("src/interactions/grow.css").text();
+    output += await Bun.file("src/interactions/rotate-into-focus.css").text();
+    output += await Bun.file("src/interactions/subtle-scale.css").text();
+
     await Bun.file("dist/main.css").write(output);
 
     let lib = "";
