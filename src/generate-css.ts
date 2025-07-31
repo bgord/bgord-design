cc8659ccd62684b1ba4729b1ba1b89e42baa4657
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-31 21:44:22 +0200

    feature: introduce layers

--- src/generate-css.ts
+++ src/generate-css.ts
@@ -3,18 +3,15 @@ import { UtilityGenerator } from "./utilities/template";
 
 export class GenerateCSS {
   static async process(tokens: TokenGenerator[], utilities: UtilityGenerator[]) {
-    let output = ":root {";
+    let output = "@layer reset, components, utilities;\n\n";
 
+    output += ":root {\n";
     for (const token of tokens) {
       output += token.getTokens();
     }
+    output += "}\n\n";
 
-    output += "}";
-
-    for (const utility of utilities) {
-      output += utility.css();
-    }
-
+    output += "@layer components {\n";
     output += await Bun.file("src/ui/button.css").text();
     output += await Bun.file("src/ui/input.css").text();
     output += await Bun.file("src/ui/label.css").text();
@@ -26,13 +23,20 @@ export class GenerateCSS {
     output += await Bun.file("src/ui/checkbox.css").text();
     output += await Bun.file("src/ui/file-explorer.css").text();
     output += await Bun.file("src/ui/range.css").text();
+    output += "}\n\n";
 
+    output += "@layer utilities {\n";
     output += await Bun.file("src/animations/grow-fade-in.css").text();
     output += await Bun.file("src/animations/shrink-fade-out.css").text();
 
     output += await Bun.file("src/interactions/grow.css").text();
     output += await Bun.file("src/interactions/rotate-into-focus.css").text();
     output += await Bun.file("src/interactions/subtle-scale.css").text();
+
+    for (const utility of utilities) {
+      output += utility.css();
+    }
+    output += "}\n\n";
 
     await Bun.file("dist/main.css").write(output);
   }
