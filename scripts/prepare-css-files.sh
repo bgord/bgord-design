e911552f6da74c37a4c0ec30a66b85402d43913b
Bartosz Gordon <gordonbartosz@gmail.com>
2025-07-31 23:35:21 +0200

    refactor: remove separate normalize.css

--- scripts/prepare-css-files.sh
+++ scripts/prepare-css-files.sh
@@ -6,12 +6,8 @@ source bgord-scripts/base.sh
 info "Building design system CSS..."
 bun dist/index.js
 
-info "Copying normalize.css to dist..."
-cp src/normalize.css dist/
-
 info "Formatting the files..."
 bunx @biomejs/biome format --write dist/main.css
-bunx @biomejs/biome format --write dist/normalize.css
 bunx @biomejs/biome format --write dist/index.d.ts
 bunx @biomejs/biome format --write dist/lib.d.ts
 bunx @biomejs/biome format --write dist/lib.js
@@ -24,21 +20,12 @@ bunx lightningcss-cli dist/main.css \
   --bundle \
   -o dist/main.min.css
 
-bunx lightningcss-cli dist/normalize.css \
-  --targets ">1%, not dead" \
-  --minify \
-  -o dist/normalize.min.css
-
 info "Compressing..."
 bunx gzip dist/*.min.css --extension=gz --extension=br
 
 info "main.css file sizes:"
 # shellcheck disable=SC2010
 ls -alh dist | grep main | awk '{print $5"\t"$9}'
-
-info "normalize.css file sizes:"
-# shellcheck disable=SC2010
-ls -alh dist | grep normalize | awk '{print $5"\t"$9}'
 
 info "Removing index.js file"
 rm -rf dist/index.js
