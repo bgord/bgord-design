79c544a2ef4e38fb56690c8776df04cc56b342fd
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-10 18:27:36 +0100

    step: use css rule in cursor

--- src/utilities/cursor-utility-generator.ts
+++ src/utilities/cursor-utility-generator.ts
@@ -1,4 +1,4 @@
-import { UtilityGenerator } from "./template";
+import { CssRule, UtilityGenerator } from "./template";
 
 export class CursorUtilityGenerator extends UtilityGenerator {
   config = { wait: "wait", auto: "auto", pointer: "pointer", "not-allowed": "not-allowed" };
@@ -8,14 +8,14 @@ export class CursorUtilityGenerator extends UtilityGenerator {
   }
 
   css() {
-    const lines: string[] = [];
+    const rules: CssRule[] = [];
 
     for (const [key, value] of Object.entries(this.config)) {
-      lines.push(`[data-cursor='${key}'] { cursor: ${value}; }`);
+      rules.push(new CssRule(`[data-cursor='${key}']`, [["cursor", value]]));
     }
 
     // Stryker disable all
-    return lines.join("\n");
+    return rules.map((rule) => rule.get()).join("\n");
     // Stryker restore all
   }
 
