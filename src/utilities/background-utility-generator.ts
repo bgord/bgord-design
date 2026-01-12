e26a2b93969bc664c5846e0fc4d6f05c58cdf4e8
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-12 15:31:27 +0100

    step: add responsive utilities for background

--- src/utilities/background-utility-generator.ts
+++ src/utilities/background-utility-generator.ts
@@ -41,6 +41,24 @@ export class BackgroundUtilityGenerator extends UtilityGenerator {
 
     result += regular.map((rule) => rule.get()).join("\n");
 
+    for (const [name, breakpoint] of Object.entries(this.breakpointRegistry.breakpoints)) {
+      const responsive: CssRuleRegular[] = [];
+
+      result += `@media (max-width: ${breakpoint}px) { `;
+
+      for (const variable of Object.keys(this.config)) {
+        const key = variable.replace("color-", "");
+
+        responsive.push(
+          new CssRuleRegular(`[data-${name}-bg='${key}']`, ["background", `var(--${variable})`]),
+        );
+      }
+
+      result += responsive.map((rule) => rule.get()).join("\n");
+
+      result += "}";
+    }
+
     return result;
   }
 
--- tests/background-utility-generator.test.ts
+++ tests/background-utility-generator.test.ts
@@ -68,6 +68,54 @@ describe("BackgroundUtilityGenerator", () => {
       [data-bg='warning-500'] { background: var(--color-warning-500); }
       [data-bg='warning-700'] { background: var(--color-warning-700); }
       [data-bg='warning-900'] { background: var(--color-warning-900); }
+
+      @media (max-width: 768px) {
+        [data-md-bg='neutral-0'] { background: var(--color-neutral-0); }
+        [data-md-bg='neutral-50'] { background: var(--color-neutral-50); }
+        [data-md-bg='neutral-100'] { background: var(--color-neutral-100); }
+        [data-md-bg='neutral-200'] { background: var(--color-neutral-200); }
+        [data-md-bg='neutral-300'] { background: var(--color-neutral-300); }
+        [data-md-bg='neutral-400'] { background: var(--color-neutral-400); }
+        [data-md-bg='neutral-500'] { background: var(--color-neutral-500); }
+        [data-md-bg='neutral-600'] { background: var(--color-neutral-600); }
+        [data-md-bg='neutral-700'] { background: var(--color-neutral-700); }
+        [data-md-bg='neutral-800'] { background: var(--color-neutral-800); }
+        [data-md-bg='neutral-900'] { background: var(--color-neutral-900); }
+        [data-md-bg='neutral-950'] { background: var(--color-neutral-950); }
+
+        [data-md-bg='brand-50'] { background: var(--color-brand-50); }
+        [data-md-bg='brand-100'] { background: var(--color-brand-100); }
+        [data-md-bg='brand-200'] { background: var(--color-brand-200); }
+        [data-md-bg='brand-300'] { background: var(--color-brand-300); }
+        [data-md-bg='brand-400'] { background: var(--color-brand-400); }
+        [data-md-bg='brand-500'] { background: var(--color-brand-500); }
+        [data-md-bg='brand-600'] { background: var(--color-brand-600); }
+        [data-md-bg='brand-700'] { background: var(--color-brand-700); }
+        [data-md-bg='brand-800'] { background: var(--color-brand-800); }
+        [data-md-bg='brand-900'] { background: var(--color-brand-900); }
+
+        [data-md-bg='positive-0'] { background: var(--color-positive-0); }
+        [data-md-bg='positive-100'] { background: var(--color-positive-100); }
+        [data-md-bg='positive-200'] { background: var(--color-positive-200); }
+        [data-md-bg='positive-400'] { background: var(--color-positive-400); }
+        [data-md-bg='positive-600'] { background: var(--color-positive-600); }
+        [data-md-bg='positive-800'] { background: var(--color-positive-800); }
+        [data-md-bg='positive-900'] { background: var(--color-positive-900); }
+
+        [data-md-bg='danger-0'] { background: var(--color-danger-0); }
+        [data-md-bg='danger-100'] { background: var(--color-danger-100); }
+        [data-md-bg='danger-200'] { background: var(--color-danger-200); }
+        [data-md-bg='danger-400'] { background: var(--color-danger-400); }
+        [data-md-bg='danger-600'] { background: var(--color-danger-600); }
+        [data-md-bg='danger-800'] { background: var(--color-danger-800); }
+        [data-md-bg='danger-900'] { background: var(--color-danger-900); }
+
+        [data-md-bg='warning-100'] { background: var(--color-warning-100); }
+        [data-md-bg='warning-300'] { background: var(--color-warning-300); }
+        [data-md-bg='warning-500'] { background: var(--color-warning-500); }
+        [data-md-bg='warning-700'] { background: var(--color-warning-700); }
+        [data-md-bg='warning-900'] { background: var(--color-warning-900); }
+      }
     `);
 
     expect(generator.toTypeScript()).toEqualIgnoringWhitespace(`
@@ -138,6 +186,56 @@ describe("BackgroundUtilityGenerator", () => {
       [data-bg='warning-500'] { background: var(--color-warning-500); }
       [data-bg='warning-700'] { background: var(--color-warning-700); }
       [data-bg='warning-900'] { background: var(--color-warning-900); }
+
+      @media (max-width: 768px) {
+        [data-md-bg='neutral-0'] { background: var(--color-neutral-0); }
+        [data-md-bg='neutral-50'] { background: var(--color-neutral-50); }
+        [data-md-bg='neutral-100'] { background: var(--color-neutral-100); }
+        [data-md-bg='neutral-200'] { background: var(--color-neutral-200); }
+        [data-md-bg='neutral-300'] { background: var(--color-neutral-300); }
+        [data-md-bg='neutral-400'] { background: var(--color-neutral-400); }
+        [data-md-bg='neutral-500'] { background: var(--color-neutral-500); }
+        [data-md-bg='neutral-600'] { background: var(--color-neutral-600); }
+        [data-md-bg='neutral-700'] { background: var(--color-neutral-700); }
+        [data-md-bg='neutral-800'] { background: var(--color-neutral-800); }
+        [data-md-bg='neutral-900'] { background: var(--color-neutral-900); }
+        [data-md-bg='neutral-950'] { background: var(--color-neutral-950); }
+
+        [data-md-bg='foo'] { background: var(--color-foo); }
+
+        [data-md-bg='brand-50'] { background: var(--color-brand-50); }
+        [data-md-bg='brand-100'] { background: var(--color-brand-100); }
+        [data-md-bg='brand-200'] { background: var(--color-brand-200); }
+        [data-md-bg='brand-300'] { background: var(--color-brand-300); }
+        [data-md-bg='brand-400'] { background: var(--color-brand-400); }
+        [data-md-bg='brand-500'] { background: var(--color-brand-500); }
+        [data-md-bg='brand-600'] { background: var(--color-brand-600); }
+        [data-md-bg='brand-700'] { background: var(--color-brand-700); }
+        [data-md-bg='brand-800'] { background: var(--color-brand-800); }
+        [data-md-bg='brand-900'] { background: var(--color-brand-900); }
+
+        [data-md-bg='positive-0'] { background: var(--color-positive-0); }
+        [data-md-bg='positive-100'] { background: var(--color-positive-100); }
+        [data-md-bg='positive-200'] { background: var(--color-positive-200); }
+        [data-md-bg='positive-400'] { background: var(--color-positive-400); }
+        [data-md-bg='positive-600'] { background: var(--color-positive-600); }
+        [data-md-bg='positive-800'] { background: var(--color-positive-800); }
+        [data-md-bg='positive-900'] { background: var(--color-positive-900); }
+
+        [data-md-bg='danger-0'] { background: var(--color-danger-0); }
+        [data-md-bg='danger-100'] { background: var(--color-danger-100); }
+        [data-md-bg='danger-200'] { background: var(--color-danger-200); }
+        [data-md-bg='danger-400'] { background: var(--color-danger-400); }
+        [data-md-bg='danger-600'] { background: var(--color-danger-600); }
+        [data-md-bg='danger-800'] { background: var(--color-danger-800); }
+        [data-md-bg='danger-900'] { background: var(--color-danger-900); }
+
+        [data-md-bg='warning-100'] { background: var(--color-warning-100); }
+        [data-md-bg='warning-300'] { background: var(--color-warning-300); }
+        [data-md-bg='warning-500'] { background: var(--color-warning-500); }
+        [data-md-bg='warning-700'] { background: var(--color-warning-700); }
+        [data-md-bg='warning-900'] { background: var(--color-warning-900); }
+      }
     `);
     expect(generator.toTypeScript()).toEqualIgnoringWhitespace(`
       "data-bg"?: "neutral-0" | "neutral-50" | "neutral-100" | "neutral-200" | "neutral-300" | "neutral-400" | "neutral-500" | "neutral-600" | "neutral-700" | "neutral-800" | "neutral-900" | "neutral-950" | "foo" | "brand-50" | "brand-100" | "brand-200" | "brand-300" | "brand-400" | "brand-500" | "brand-600" | "brand-700" | "brand-800" | "brand-900" | "positive-0" | "positive-100" | "positive-200" | "positive-400" | "positive-600" | "positive-800" | "positive-900" | "danger-0" | "danger-100" | "danger-200" | "danger-400" | "danger-600" | "danger-800" | "danger-900" | "warning-100" | "warning-300" | "warning-500" | "warning-700" | "warning-900";
