3504a61f50ddf91fdaf178f924f1af2fccea65f5
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-12 15:55:43 +0100

    step: add responsive utilities for pointer events

--- src/utilities/pointer-event-utility-generator.ts
+++ src/utilities/pointer-event-utility-generator.ts
@@ -20,6 +20,22 @@ export class PointerEventUtilityGenerator extends UtilityGenerator {
 
     result += regular.map((rule) => rule.get()).join("\n");
 
+    for (const [name, breakpoint] of Object.entries(this.breakpointRegistry.breakpoints)) {
+      const responsive: CssRuleRegular[] = [];
+
+      result += `@media (max-width: ${breakpoint}px) { `;
+
+      for (const [key, value] of Object.entries(this.config)) {
+        responsive.push(
+          new CssRuleRegular(`[data-${name}-pointer-events='${key}']`, ["pointer-events", value]),
+        );
+      }
+
+      result += responsive.map((rule) => rule.get()).join("\n");
+
+      result += "}";
+    }
+
     return result;
   }
 
--- tests/pointer-event-utility-generator.test.ts
+++ tests/pointer-event-utility-generator.test.ts
@@ -12,6 +12,11 @@ describe("PointerEventUtilityGenerator", () => {
     expect(generator.css()).toEqualIgnoringWhitespace(`
       [data-pointer-events='none'] { pointer-events: none; }
       [data-pointer-events='auto'] { pointer-events: auto; }
+
+      @media (max-width: 768px) {
+        [data-md-pointer-events='none'] { pointer-events: none; }
+        [data-md-pointer-events='auto'] { pointer-events: auto; }
+      }
     `);
     expect(generator.toTypeScript()).toEqualIgnoringWhitespace(`
       "data-pointer-events"?: "none" | "auto";
