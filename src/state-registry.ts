573f220f5e239585d6fa1f0312e23b803bc8eed0
Bartosz Gordon <gordonbartosz@gmail.com>
2026-01-14 13:41:04 +0100

    fix: mutation tests

--- src/state-registry.ts
+++ src/state-registry.ts
@@ -2,8 +2,10 @@ export class StateRegistry {
   registry: { hover?: string; focus?: string; active?: string } = {};
 
   constructor(states: { hover?: boolean; focus?: boolean; active?: boolean }) {
+    // Stryker disable all
     if (states.hover) this.registry.hover = ":hover:not(:disabled)";
     if (states.focus) this.registry.focus = ":focus-visible";
+    // Stryker restore all
     if (states.active) this.registry.active = ":active";
   }
 
